6ad503e3fee8d6c46399c2f5c007d79cbea5c365
Rob Rogers <160201478+Rob-Rogers@users.noreply.github.com>
2024-02-17 22:11:39 -0500

    Update passivearpscan.c
    forgot no warranty section

--- passivearpscan.c
+++ passivearpscan.c
@@ -4,6 +4,11 @@
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License 2.1
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
  */
 
 
